e5ffca34dfe53c92e9b1b727e30d6e0ca5a944d4
WedaPashi10 <tandale.prasad@gmail.com>
2022-05-16 19:58:39 +0530

    first project

---
+++ hello/target/release/deps/hello-49edc15e64cc879c.d
@@ -0,0 +1,5 @@
+/home/wedapashi/learning/rust/Rust/hello/target/release/deps/hello-49edc15e64cc879c: src/main.rs
+
+/home/wedapashi/learning/rust/Rust/hello/target/release/deps/hello-49edc15e64cc879c.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/release/hello.d
@@ -0,0 +1,1 @@
+/home/wedapashi/learning/rust/Rust/hello/target/release/hello: /home/wedapashi/learning/rust/Rust/hello/src/main.rs
